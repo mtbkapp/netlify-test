f12e021fc53377d7c80aacc122ca6db3376d5ad5
Jason Kapp <mtbkapp@gmail.com>
2020-12-28 10:24:07 -0700

    test auto deploy

--- src/main/acme/frontend/app.cljs
+++ src/main/acme/frontend/app.cljs
@@ -5,4 +5,4 @@
   (-> js/document
       (.getElementById "root")
       (.-innerHTML)
-      (set! "Hello cljs & netlify"))))
+      (set! "Hello cljs & netlify, can I local dev against netlify api?"))))
